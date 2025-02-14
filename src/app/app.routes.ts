d756c30f31a365cedcb9698df8ddf799f1008531
cotiaws <cotiaws5@gmail.com>
2025-02-13 21:44:21 -0300

    Integração com API para consulta de categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,5 +16,10 @@ export const routes: Routes = [
     {
         path: 'pages/edicao-produtos', //ROTA
         component: EdicaoProdutosComponent //COMPONENTE
+    },
+    {
+        // Rota padrão
+        path: '', pathMatch: 'full', //raiz do projeto
+        redirectTo: 'pages/consulta-produtos' //redirecionamento
     }
 ];
--- src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-produtos',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css'
 })
 export class CadastroProdutosComponent {
 
+  //atributos do componente
+  categorias: any[] = []; //array de objetos
+
+  //método construtor (inicializar objetos)
+  constructor(private http: HttpClient) {}
+
+  //função executada ao abrir a página
+  ngOnInit() {
+    
+    //fazendo uma requisição GET para a API
+    this.http.get('http://localhost:8080/api/categorias/consultar')
+      .subscribe({ //capturando a resposta da requisição
+        next: (data) => { //se a requisição for bem sucedida
+          this.categorias = data as any[];
+        }
+      });
+  }
 }
